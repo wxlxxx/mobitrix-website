f647e3fc49e54a3813d098ba68ab6abee1f94fe8
wxlxxx <964397251@qq.com>
2020-06-10 00:55:23 +0800

    wxl

--- src/script/PurchasePage.js
+++ src/script/PurchasePage.js
@@ -1,7 +1,15 @@
 import countdown from 'countdown'
+import Cookies from 'js-cookie'
 
+let end;
+if(Cookies.get('countdownEnd') && Cookies.get('countdownEnd') > new Date().getTime()){
+  end = Cookies.get('countdownEnd')
+}else {
+  end = new Date().getTime()+1000*2*24*60*60
+}
+Cookies.set('countdownEnd', end)
 function update(){
-  const ts = countdown(new Date(2020, 5, 30) )
+  const ts = countdown(end)
   document.querySelectorAll('#days')[0].innerText = ts.days < 10 ? '0'+ts.days : ts.days
   document.querySelectorAll('#hours')[0].innerText = ts.hours < 10 ? '0'+ts.hours : ts.hours
   document.querySelectorAll('#minutes')[0].innerText = ts.minutes < 10 ? '0'+ts.minutes : ts.minutes
